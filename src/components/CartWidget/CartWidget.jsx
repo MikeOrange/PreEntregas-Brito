b6261b67a85c946ab86578b8b6ad08eb8dd73030
Miguel Brito <miguelbrito@MacBook-Air-de-Miguel.local>
2023-10-21 23:56:13 -0400

    Usando icono de carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { FaShoppingCart } from 'react-icons/fa';
 import './CartWidget.css'
 
 function CartWidget({count}) {
   return (
     <span>
       <span className='cart-count'>1</span>
-      <span>Icon</span>
+      <span><FaShoppingCart /></span>
     </span>
   )
 }
