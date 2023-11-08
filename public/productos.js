f1a0cae2e8a0fad0281e3e011c5a0502b5e3bb88
Miguel Brito <miguelandres77@gmail.com>
2023-11-08 20:28:34 -0300

    Anadiendo filtrado por categoria

--- public/productos.js
+++ public/productos.js
@@ -5,6 +5,7 @@
         "description": "Curso de Python",
         "price": 10.00,
         "pictureUrl": "/fotos/foto1.png",
+        "category": "backend",
         "stock": 10
     },
     {
@@ -13,6 +14,7 @@
         "description": "Curso de JS",
         "price": 15.00,
         "pictureUrl": "/fotos/foto2.png",
+        "category": "frontend",
         "stock": 7
     },
     {
@@ -21,6 +23,7 @@
         "description": "Curso de Java",
         "price": 10.00,
         "pictureUrl": "/fotos/foto3.png",
+        "category": "backend",
         "stock": 1
     },
     {
@@ -29,6 +32,7 @@
         "description": "Curso de PHP",
         "price": 15.00,
         "pictureUrl": "/fotos/foto4.png",
+        "category": "backend",
         "stock": 0
     },
     {
@@ -37,6 +41,7 @@
         "description": "Curso de Typescript",
         "price": 20.00,
         "pictureUrl": "/fotos/foto5.png",
+        "category": "frontend",
         "stock": 10
     }
 ]
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,20 +1,30 @@
 import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 import Item from '../Item/Item';
 import { productAPIClient } from '../../productAPIClient';
 
 function ItemList() {
+  const {id} = useParams();
   const [productData, setProductData] = useState([]);
+  const [loadStatus, setLoadStatus] = useState(false);
   
   useEffect(() => {
     productAPIClient.getData().then((data) => {
-        setProductData(data)
+        const filteredData = id ? data.filter((elem) => elem.category === id ) : data;
+
+        setProductData(filteredData)
+        setLoadStatus(true)
     })
 
-  }, []);
+  }, [id]);
+
+  console.log(loadStatus)
 
   return (
     <div>
-        {productData.length == 0 ? <p>Cargando...</p> : productData.map((elem) => <Item key={elem.id} product={elem} />)}
+        {loadStatus 
+        ? productData.length == 0 ? <p>Pronto</p> : productData.map((elem) => <Item key={elem.id} product={elem} />)
+        : <p>Cargando...</p> }
     </div>
   )
 }
